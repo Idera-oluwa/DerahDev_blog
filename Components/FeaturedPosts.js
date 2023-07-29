43c949a551484aeca18997942a4b1cbe28d92f5f
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-07-29 00:56:41 -0700

    new commit

--- Components/FeaturedPosts.js
+++ Components/FeaturedPosts.js
@@ -23,7 +23,7 @@ const FeaturedPosts = () => {
       }, []);
   return (
     <div>
-      <div className='mt-[3rem] md:mt-[4rem] w-[85vw] mx-auto'>
+      <div className='mt-[3rem] md:mt-[4rem] w-[90vw] md:w-[75vw] lg:w-[83vw] mx-auto'>
         <h1 className='text-[1.9rem] md:text-[2.9rem] text-center font-extrabold text-[#2a2d38]'>DerahDev Blog</h1>
         <p className='text-[0.95rem] md:text-[1.25rem] text-center font-bold text-[#2a2d38] mt-[1.5rem] md:mt-[3.5rem]'>Stay up-to-date with the latest industry insights, as we explore software engineering best practices, 
           design patterns, and architectural principles that empower you to build scalable, maintainable, and
@@ -42,17 +42,17 @@ const FeaturedPosts = () => {
     >
       {dataLoaded && featuredPosts.map((post)=>{
         return(
-          <SwiperSlide>
+          <SwiperSlide className='w-full mx-auto'>
       <div className='grid md:grid-cols-3 mt-[4.5rem] mb-[4.5rem] relative'>
       <img src={post.featuredImage.url}
-            className='md:col-span-2 w-[90%] h-[14rem] md:h-[25rem]'
+            className='md:col-span-2 w-[100%] md:w-[90%] h-[14rem] md:h-[25rem]'
             alt={post.title}/>
            <div className=' md:col-span-1 flex flex-col justify-center mt-[2rem] md:mt-[0rem]'>
               <button className='w-[8.2rem] h-[2.5rem] bg-[#005cff] text-center mr-auto text-white text-[0.85rem] font-bold'>FEATURED</button>
               <Link href={`/post/${post.slug}`}><h1 className='font-bold text-[2.3rem] leading-[2.9rem] mt-[0.5rem] cursor-pointer'>{post.title}</h1></Link>
             <p className='mt-[0.5rem] text-[0.8rem] text-[#909ba5]'>By <span className='text-[#005cff] font-bold'>{post.authur.name}</span> | {moment(post.createdAt).format('MMM DD, YYYY')}</p>
             </div>
-            <div className='absolute left-0 right-0 top-[50%] flex justify-between'>
+            <div className='absolute left-0 hidden right-0 top-[50%] md:flex justify-between'>
               <FiArrowLeftCircle className='text-[#909ba5] text-[1.6rem] hover:text-[#005cff] cursor-pointer'
                onClick={() => sliderRef.current?.slidePrev()}
               />
