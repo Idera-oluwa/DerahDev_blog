38220e67bec7fe9d0eda6e5aabffffa3d0e52980
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-07-29 03:37:37 -0700

    new commit

--- Components/RecentPosts.js
+++ Components/RecentPosts.js
@@ -13,13 +13,13 @@ const RecentPosts = () => {
   console.log(posts)
 
   return (
-    <div className='mt-[4rem] w-[85vw] mx-auto mb-[5rem]'>
+    <div className='mt-[4rem] w-[90vw] md:w-[75vw] lg:w-[83vw] mx-auto mb-[5rem]'>
       <h1 className='text-[1.5rem] md:text-[2.5rem] text-center font-extrabold text-[#2a2d38]'>Latest Posts</h1>
       <div className='w-[3rem] h-[0.15rem] bg-black mb-[1.8rem] md:mb-[2.3rem] mx-auto mt-[0.9rem] md:mt-[1.3rem]'></div>
-      <div className='grid grid-cols-fluid gap-[2.5rem]'>
+      <div className='grid grid-cols-fluid gap-[2.3rem]'>
         {posts.map((post)=>{
         return(
-          <div key={post.node.slug} className='w-full mx-auto'>
+          <div key={post.node.slug} className='container mx-auto'>
             <img src={post.node.featuredImage.url} className='min-w-[100%] max-w-[100%] h-[16rem] md:h-[21.5rem]'/>
             <button className='bg-black text-white p-[0.3rem] text-[0.7rem] font-semibold mt-[1.2rem]'>{post.node.categories[0].name}</button>
             <Link href={`/post/${post.node.slug}`}><h1 className='text-[1.5rem] md:text-[1.65rem]  leading-[1.8rem] font-bold text-[#2a2d38] mt-[0.8rem]'>{post.node.title}</h1></Link>
