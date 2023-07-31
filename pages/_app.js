5bb88602a99562efa3b2b92a55632789313800a6
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-07-31 05:57:49 -0700

    new commmit

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,31 @@ import NextProgress from "next-progress";
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <NextSeo
+            title='Derah Blog'
+            description="Next SEO packages simplifies the SEO management in Next Apps with less configurations"
+            canonical="www.example.com/next-seo-blog"
+            openGraph={{
+                type: 'article',
+                article: {
+                    publishedTime: '2022-06-21T23:04:13Z',
+                    modifiedTime: '2022-01-21T18:04:43Z',
+                    authors: [
+                        'https://www.example.com/authors/@firstnameA-lastnameA',
+                        'https://www.example.com/authors/@firstnameB-lastnameB',
+                    ],
+                    tags: ['Tag A', 'Tag B', 'Tag C'],
+                },
+                url: 'www.example.com/next-seo-blog',
+                images: {
+                    url: 'Images/dev.jpg' ,
+                    width: 850,
+                    height: 650,
+                    alt: 'Photo of text',
+                },
+                site_name: 'Next Blog'
+            }}
+        />
     <NextProgress delay={300} options={{ showSpinner: false }} />
    <Layout>
   <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,6 @@ export default function Home() {
   <div>
   <Head>
         <title>Derahdev Blog</title>
-        <Head>
-      <meta property="og:title" content='Derahdev blog'/>
-        <meta property="og:description" content='An exclusive website' />
-        <meta property="og:image" content='Images/dev.jpg' />
-        <meta property="og:type" content="article" />
-        <link rel="icon" href="Images/dev.jpg" />
-        {/* <meta property="og:url" content="URL of the page you're sharing" /> */}
-      </Head>
       </Head>
     <Hero/>
   </div>
