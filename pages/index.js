98918e28ab43b81aeb7785689ae9ad0de5b73a9c
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-07-31 06:50:21 -0700

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import React from 'react';
 import Hero from './Hero'
+import {NextSeo} from 'next-seo';
 export default function Home() {
   return (
     <>
@@ -8,6 +9,31 @@ export default function Home() {
   <Head>
         <title>Derahdev Blog</title>
       </Head>
+      <NextSeo
+            title='Derah Blog'
+            description="Next SEO packages simplifies the SEO management in Next Apps with less configurations"
+            canonical="www.example.com/next-seo-blog"
+            openGraph={{
+                type: 'article',
+                article: {
+                    publishedTime: '2022-06-21T23:04:13Z',
+                    modifiedTime: '2022-01-21T18:04:43Z',
+                    authors: [
+                        'https://www.example.com/authors/@firstnameA-lastnameA',
+                        'https://www.example.com/authors/@firstnameB-lastnameB',
+                    ],
+                    tags: ['Tag A', 'Tag B', 'Tag C'],
+                },
+                url: 'www.example.com/next-seo-blog',
+                images: {
+                    url: 'Images/dev.jpg' ,
+                    width: 850,
+                    height: 650,
+                    alt: 'Photo of text',
+                },
+                site_name: 'Next Blog'
+            }}
+        />
     <Hero/>
   </div>
     </>
